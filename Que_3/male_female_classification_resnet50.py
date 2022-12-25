2a277e04be623146aa0ab1297d9dc3668c32e725
Neetiraj Malviya <101549053+NEETIRAJ-MALVIYA@users.noreply.github.com>
2022-12-25 06:28:26 +0530

    Add files via upload

---
+++ Que_3/male_female_classification_resnet50.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Dropout
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+# In[2]:
+
+
+num_classes = 2
+
+
+# In[3]:
+
+
+from tensorflow.keras.optimizers import Adam
+
+
+# In[4]:
+
+
+# load the ResNet50 model and add a few layers on top
+model = Sequential()
+model.add(ResNet50(include_top=False, pooling='avg', weights='imagenet'))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+
+# In[5]:
+
+
+for layer in model.layers[0].layers:
+    layer.trainable = False
+
+
+# In[6]:
+
+
+# compile the model
+model.compile(optimizer=Adam(lr = 0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+# # Augumented data
+
+# In[7]:
+
+
+# define the data generators for training, validation, and test sets
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
+
+
+# In[8]:
+
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+
+# In[9]:
+
+
+train_dir = r"C:\Users\Neetiraj\Videos\archive\traindata\traindata"
+val_dir = r"C:\Users\Neetiraj\Videos\archive\testdata\testdata"
+
+
+# In[10]:
+
+
+# create the generators for the training, validation, and test sets
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    target_size=(224, 224),
+                                                    batch_size=32,
+                                                    class_mode='categorical')
+
+
+# In[11]:
+
+
+validation_generator = validation_datagen.flow_from_directory(val_dir,
+                                                              target_size=(224, 224),
+                                                              batch_size=32,
+                                                              class_mode='categorical')
+
+
+# In[12]:
+
+
+# train the model
+history = model.fit_generator(train_generator,
+                              epochs=10,
+                              validation_data=validation_generator,
+                              steps_per_epoch=len(train_generator),
+                              validation_steps=len(validation_generator))
+
+
+# In[13]:
+
+
+import matplotlib.pyplot as plt
+plt.figure(figsize =(10,8))
+plt.plot(history.history['loss'],label= 'loss')
+plt.plot(history.history['val_loss'], label = 'validation_loss')
+plt.plot(history.history['accuracy'], label = 'accuracy')
+plt.plot(history.history['val_accuracy'], label = 'validation_accuracy')
+plt.legend()
+plt.show()
+
+
+# # Without Augumentaion
+
+# In[14]:
+
+
+# define the data generators for training, validation, and test sets
+train_datagen = ImageDataGenerator(rescale=1./255)
+validation_datagen = ImageDataGenerator(rescale=1./255)
+train_dir = r"C:\Users\Neetiraj\Videos\archive\traindata\traindata"
+val_dir = r"C:\Users\Neetiraj\Videos\archive\testdata\testdata"
+
+
+# In[15]:
+
+
+# create the generators for the training, validation, and test sets
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    target_size=(224, 224),
+                                                    batch_size=32,
+                                                    class_mode='categorical')
+# create the generators for the training, validation, and test sets
+val_generator = validation_datagen.flow_from_directory(val_dir,
+                                                    target_size=(224, 224),
+                                                    batch_size=32,
+                                                    class_mode='categorical')
+
+
+# In[16]:
+
+
+# train the model
+history = model.fit_generator(train_generator,
+                              epochs=10,
+                              validation_data=validation_generator,
+                              steps_per_epoch=len(train_generator),
+                              validation_steps=len(validation_generator))
+
+
+# In[17]:
+
+
+import matplotlib.pyplot as plt
+plt.figure(figsize =(10,8))
+plt.plot(history.history['loss'],label= 'loss')
+plt.plot(history.history['val_loss'], label = 'validation_loss')
+plt.plot(history.history['accuracy'], label = 'accuracy')
+plt.plot(history.history['val_accuracy'], label = 'validation_accuracy')
+plt.legend()
+plt.show()
+
+
+# In[ ]:
+
+
+
+
